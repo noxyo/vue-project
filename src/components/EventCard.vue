ebb345d16913fcbaf6ea6dc85d9f9eab5282515d
noxyo <zetinocodavila@gmail.com>
2019-02-28 07:24:25 +0000

    theme updates

---
+++ src/components/EventCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 v-if @click="handler(arg, event)"></h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'i want to be single'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
